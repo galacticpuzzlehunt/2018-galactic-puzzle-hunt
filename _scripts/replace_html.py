bdad2cadf3986adff55300e761e805a6b76ac3de
Rahul Sridhar <sridhar.rahul@gmail.com>
2018-06-12 17:56:20 -0700

    capital I for Incorrect!

--- _scripts/replace_html.py
+++ _scripts/replace_html.py
@@ -1,49 +1,8 @@
 import os
 import argparse
 
-text = r'''
-    <form
-        class="call-in-answer-panel inline-form"
-    >'''
-replacement = r'''    <script id="check_answer">
-      window.onload=function() {
-        form = document.getElementsByClassName('call-in-answer-panel inline-form')[0]
-        form.onsubmit=function() {
-          right_answer = ''
-          answer = document.getElementById('id_answer').value
-          answer = normalize_puzzle_answer(answer)
-
-          // Remove solved panel if it exists
-          if (document.contains(document.getElementById('solved-panel'))) {
-            document.getElementById("solved-panel").remove();
-          }
-
-          // Create new solved panel
-          node = document.createElement('div')
-          node.setAttribute('id', 'solved-panel')
-          if (answer == right_answer) {
-            node.setAttribute('class', 'solved-panel')
-            node.innerHTML = "<h3>Solved!</h3>\n<p>The answer was <b>" + right_answer + "</b>.</p>"
-          } else {
-            node.setAttribute('class', 'solved-panel solved-panel--incorrect')
-            node.innerHTML = "<h3>incorrect!</h3>"
-          }
-
-          // Inject it
-          form.parentNode.insertBefore(node, form)
-
-          // Return false to prevent GET request from executing
-          return false;
-        }
-      }
-
-      function normalize_puzzle_answer(answer) {
-        return answer.toUpperCase().replace(/[^A-Z]/g, '')
-      }
-    </script>
-    <form
-            class="call-in-answer-panel inline-form"
-        >'''
+text = r'''<h3>incorrect!</h3>'''
+replacement = r'''<h3>Incorrect!</h3>'''
 
 
 
