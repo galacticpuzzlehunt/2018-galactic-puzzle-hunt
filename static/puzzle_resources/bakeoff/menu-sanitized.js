10a9e5ca447413d5a0d8fd2ad7a9c13b79c6cd6b
Rahul Sridhar <sridhar.rahul@gmail.com>
2018-06-15 20:03:40 -0700

    bakeoff fixed (probably)

---
+++ static/puzzle_resources/bakeoff/menu-sanitized.js
@@ -0,0 +1,467 @@
+var text = {
+    "menuPages": [
+      {
+          "header": "Rolfobben",
+          "name": "Trackle",
+          "description": "Fresher and more delicious than store-bought.",
+          "steps": [
+              "Add 2-3 weebles to 3 cups okk, and knead together.",
+              "Roll out mixture with a rolling pin into a flat sheet and cut into thin strips."
+          ]
+      },
+      {
+          "name": "Balorbb",
+          "description": "A basic, versatile balorbb.",
+          "steps": [
+              "Add 4g zam and 1g laxxloffer to 1 cup okk. Add 3/4 cup zzorp to the mixture and knead together.",
+              "Let rest overnight to rise."
+          ]
+      },
+      {
+          "name": "Twipp Balorbb",
+          "description": "Use this to make a perfect crenn!",
+          "steps": [
+              "Add 3g zam and 3g twipp to 1 cup okk.",
+              "Add 1/2 cup cold diced hompliss and roughly work it in, leaving small chunks.",
+              "Add 30g cold zzorp and mix."
+          ]
+      },
+      {
+          "name": "Worbly",
+          "description": "Have more gom than you can eat? Here&rsquo;s what to do with it.",
+          "steps": [
+              "Add 4 cups of the juice of the desired summer gom (aluupagom, sizzagom, etc.) and 3 cups twipp to a saucepan.",
+              "Boil until thickened and syrupy. Put into jars."
+          ]
+      },
+      {
+          "header": "Nibben",
+          "name": "Ozz-Moffkay",
+          "description": "A great healthy way to start the day.",
+          "steps": [
+              "Combine 1/2 cup moffkay, 1 cup pliss, 15 g twipp, zam, and atharr in a small pot.",
+              "Bring the mixture to a boil, stirring occasionally, and then take the pot off the heat. The ozz-moffkay will thicken as it cools."
+          ]
+      },
+      {
+          "name": "Weebles on Glarb",
+          "description": "Don&rsquo;t overcook the weebles; you want them to be light and fluffy.",
+          "steps": [
+              "Obtain some glarb (see recipe), and lightly toast 2 slices.",
+              "Melt 1 tablespoon hompliss in a small saute pan. Beat together 3 weebles, 2 tablespoons pliss, and zam, and add the mixture to the pan. Cook over low heat, stirring frequently.",
+              "When the weeble mixture is almost set, divide it and pile it on top of the slices of glarb."
+          ]
+      },
+      {
+          "header": "Kozzobben",
+          "name": "Trackle and Zampliss",
+          "description": "Comfort food at its best.",
+          "steps": [
+              "Obtain 1 lb of trackle (see recipe), cook it, and put it in a baking pan.",
+              "Melt 8 tablespoons hompliss in a saucepan and whisk in 1/2 cup okk.",
+              "Whisk in 6 cups pliss and 2 lb zampliss.",
+              "Pour the mixture over the baking pan, and bake until zampliss mixture is bubbly and golden."
+          ]
+      },
+      {
+          "name": "Piital",
+          "description": "Feel free to experiment with shapes and toppings.",
+          "steps": [
+              "Obtain some balorbb (see recipe) and roll it out with a rolling pin.",
+              "Top with a layer of zammgom.",
+              "Top with another layer of zampliss.",
+              "Bake until balorbb is cooked."
+          ]
+      },
+      {
+          "name": "Tok-Hompliss and Worbly",
+          "description": "Only for grown-ups with the most discerning of palates.",
+          "steps": [
+              "Obtain some glarb (see recipe) and slice it.",
+              "Apply worbly (see recipe) to one slice. Add tok-hompliss to the other. Combine slices."
+          ]
+      },
+      {
+          "header": "Spibben",
+          "name": "Glarb",
+          "description": "Perfectly soft and fluffy, this loaf will be gone before you know it.",
+          "steps": [
+              "Make the recipe for balorbb, but additionally add 1/4 cup hompliss and 1/8 cup twipp.",
+              "Form into a loaf and put into a baking pan. Bake until done."
+          ]
+      },
+      {
+          "name": "Mashed Rannuu",
+          "description": "Don&rsquo;t worry about leftovers; if you have any, you can make rannuu sammaglam.",
+          "steps": [
+              "Peel 2 lb rannuu and cut into chunks.",
+              "Boil in zzorp, then drain and mash.",
+              "Add 2 tablespoons hompliss, 1 cup pliss, and zam."
+          ]
+      },
+      {
+          "name": "Fried Rannuu",
+          "description": "Make sure to cut the strips evenly so they cook evenly.",
+          "steps": [
+              "Peel 2 lb rannuu and cut into strips.",
+              "Fry until golden and crispy. Add zam."
+          ]
+      },
+      {
+          "name": "Rannuu Sammaglam",
+          "description": "A great way to use up leftover mashed rannuu.",
+          "steps": [
+              "Obtain 2 cups mashed rannuu (see recipe).",
+              "Add additional 1 tablespoon pliss, 1 weeble, and zam.",
+              "Divide mixture into small mounds.",
+              "Dip mounds in weeble, then roll them in okk. Fry until the the outer coating is golden and crispy."
+          ]
+      },
+      {
+          "header": "Twippibben",
+          "name": "Atharr Rolls",
+          "description": "The atharr makes all the difference.",
+          "steps": [
+              "Obtain some balorbb (see recipe).",
+              "Roll out into a thick sheet and top with melted hompliss.",
+              "Combine 10g atharr with 1/3 cup twipp. Top balorbb with mixture. Roll mixture into log, slice into rounds, and put in baking dish.",
+              "Combine 1 cup twipp, 15g hompliss, and 8g pliss. Pour mixture over dish. Bake until finished."
+          ]
+      },
+      {
+          "name": "Jarpagom Crenn",
+          "description": "Feel free to make this at any time of the year.",
+          "steps": [
+              "Obtain some twipp balorbb (see recipe). Roll out with a rolling pin into a circle, and put in a baking dish.",
+              "Combine 5g zam, 10g atharr, 5g hemmarr, and 1g euusarr (to form jarpagom crenn spice). Combine that with 1 cup twipp and 8g okk.",
+              "Add 3 weebles, 500g jarpagom, and 250g pampliss and mix together.",
+              "Pour the mixture into the dish. Bake until the filling is cooked."
+          ]
+      },
+      {
+          "name": "Ozz-Moffkay Tok-Hompliss Rortwipp Toreils",
+          "description": "You won&rsquo;t find a more delicious toreil anywhere in the galaxy.",
+          "steps": [
+              "Combine 2 cups twipp, 3/4 cup tok-hompliss, 1 cup hompliss, 4g zam, and 8g laxxloffer.",
+              "Add 2 weebles.",
+              "Add 2 cups okk, 1 1/2 cups moffkay, and 340g rortwipp chips.",
+              "Divide mixture into small mounds. Bake until set and lightly golden."
+          ]
+      }
+  ],
+
+    "aliases": {
+        "baking soda": "leavening agent",
+        "baking powder": "leavening agent",
+        "yeast": "leavening agent",
+        "brie": "cheese",
+        "bleu": "cheese",
+        "cheddar": "cheese",
+        "colby": "cheese",
+        "gouda": "cheese",
+        "havarti": "cheese",
+        "jack": "cheese",
+        "mozzarella": "cheese",
+        "muenster": "cheese",
+        "parmesan": "cheese",
+        "swiss": "cheese",
+        "cherry": "cherries",
+        "potato": "potatoes",
+        "strawberry": "berries",
+        "raspberry": "berries",
+        "blueberry": "berries",
+        "blackberry": "berries",
+        "strawberries": "berries",
+        "raspberries": "berries",
+        "blueberries": "berries",
+        "blackberries": "berries",
+        "berry": "berries",
+        "tomato": "tomatoes",
+        "lemon": "lemons",
+        "cherry": "cherries",
+        "orange": "oranges",
+        "citron": "citrons"
+    },
+
+    "examineStrings": {
+        "brandy": "It&rsquo;s in a nondescript bottle from the liquor cabinet.",
+        "butter": "Sweet butter. It&rsquo;s still a bit cold.",
+        "cheese": "It&rsquo;s gooey, melty, tangy, salty cheese!",
+        "cherries": "The smell reminds you of ice cream sodas.",
+        "chocolate": "Reads &lsquo;Gobbagox Rortwipp&rsquo;, with a picture of an alien picking a cacao bean. You wonder if it&rsquo;s fair trade.",
+        "cinnamon": "It&rsquo;s in a shaker labeled &lsquo;atharr&rsquo;.",
+        "cloves": "They&rsquo;re in a shaker labeled &lsquo;euusarr&rsquo;.",
+        "cream": "It&rsquo;s part of the crop?",
+        "eggs": "Some brown chicken eggs. Or...at least you assume they&rsquo;re chickens?",
+        "flour": "Some bleached wheat flour. Looks pretty normal.",
+        "garlic": "Ever had garlic ice cream? Not the best use of garlic. Or ice cream.",
+        "kale": "Nature&rsquo;s little superfood, or so the supermarket says.",
+        "leavening agent": "Your baked goods will rise beautifully!",
+        "lemon": "The smell reminds you of summer.",
+        "mace": "It&rsquo;s not pepper spray, honest!",
+        "milk": "I see you like 1%.",
+        "nutmeg": "It&rsquo;s in a shaker labeled &rsquo;hemmarr&rsquo;.",
+        "oats": "Rolled oats. They have an earthy smell.",
+        "onions": "They&rsquo;re so beautiful, they&rsquo;re making you tear up...",
+        "oranges": "The smell reminds you of Sunday mornings.",
+        "peanut butter": "It has a picture of some alien in a peanut suit on it.",
+        "pepper": "Whole black peppercorns, contained in a nice pepper grinder.",
+        "potatoes": "Boil &rsquo;em, mash &rsquo;em, stick &rsquo;em in a stew!",
+        "pumpkins": "Ahh, smells like fall.",
+        "salt": "Kosher salt distilled from the tears of angry gamers.",
+        "sherry": "Ah, ma sherry, je t&rsquo;aime!",
+        "spinach": "You&rsquo;ve heard it gives you super strength!",
+        "berries": "Juicy and delicious.",
+        "sugar": "You&rsquo;re holding some white sugar. Sweet!",
+        "tomatoes": "Botanically fruit, culturally vegetable, certainly delicious.",
+        "water": "Some water in a cup. Looks pretty clear.",
+    },
+
+    "recipes": {
+        "crepes": [ ["flour", "eggs", "milk", "butter"], ["butter"] ],
+
+        "pasta": [ ["flour", "eggs"] ],
+        "dough": [ ["flour", "salt", "leavening agent", "water"] ],
+        "sugar dough": [ ["flour", "salt", "sugar"], ["butter"], ["water"] ],
+        "jelly": [ ["berries", "sugar"] ],
+
+        "oatmeal": [ ["oats", "milk", "sugar", "salt", "cinnamon"] ],
+        "eggs on toast": [ ["flour", "salt", "leavening agent", "butter", "sugar", "water"], ["butter"], ["eggs, milk, salt"] ],
+
+        "pasta and cheese": [ ["flour", "eggs"], ["butter"], ["flour"], ["milk", "cheese"] ],
+        "pizza": [ ["flour", "salt", "leavening agent", "water"], ["tomatoes"], ["cheese"] ],
+        "peanut butter and jelly sandwich": [ ["flour", "salt", "leavening agent", "butter", "sugar", "water"], ["berries", "sugar"], ["peanut butter"]],
+
+        "bread": [ ["flour", "salt", "leavening agent", "butter", "sugar", "water"] ],
+        "mashed potatoes": [ ["potatoes"], ["water"], ["butter", "milk", "salt"] ],
+        "French fries": [ ["potatoes"], ["salt"] ],
+        "potato croquettes": [ ["potatoes"], ["water"], ["butter", "milk", "salt"], ["milk", "eggs", "salt"], ["eggs"], ["flour"] ],
+
+        "cinnamon rolls": [ ["flour", "salt", "leavening agent", "water"], ["butter"], ["cinnamon", "sugar"], ["sugar", "butter", "milk"] ],
+        "pumpkin pie": [ ["flour", "salt", "sugar"], ["butter"], ["water"], ["salt", "cinnamon", "nutmeg", "cloves", "sugar", "flour"], ["eggs", "pumpkin", "cream"] ],
+        "cookies": [ ["sugar", "peanut butter", "butter", "salt", "leavening agent"], ["eggs"], ["flour", "oats", "chocolate"] ]
+    }
+}
+
+var menuPosition = 0;
+var ingredients = [];
+var guessLog = [];
+
+$(document).ready(function() {
+
+$(".display-help-btn").click(function() {
+    $("#help-container").fadeIn();
+    $("#help-modal").slideDown();
+});
+$(".display-log-btn").click(function() {
+    generateLogInfo();
+    $("#log-container").fadeIn();
+    $("#log-modal").slideDown();
+});
+
+$("#help-container").on('click', function(e) {
+    if (e.target.id == "help-modal" || $(e.target).parents("#help-modal").length) {
+        return;
+    }
+    $("#help-container").fadeOut();
+    $("#help-modal").slideUp();
+});
+
+$("#log-container").on('click', function(e) {
+    if (e.target.id == "log-modal" || $(e.target).parents("#log-modal").length) {
+        return;
+    }
+    $("#log-container").fadeOut();
+    $("#log-modal").slideUp();
+});
+
+function refreshMenuDisplay() {
+    var pageToRender = text.menuPages[menuPosition];
+    if ("header" in pageToRender) {
+        $(".menu-header").html(pageToRender["header"]);
+    } else {
+        $(".menu-header").html("<span style=\"display: inline-block;\"></span>");
+    }
+    var recipeName = "<h4>" + pageToRender["name"] + "</h4>";
+    var recipeDescription = "";
+    if ("description" in pageToRender) {
+        recipeDescription = "<p><i>" + pageToRender["description"] + "</i></p>";
+    }
+    var recipeSteps = "<ul>" + pageToRender["steps"].map(s => "<li>" + s + "</li>").join("") + "</ul>";
+
+    $(".instructions").html(recipeName + recipeDescription + recipeSteps);
+}
+
+function refreshIngredientsDisplay() {
+    var buildString = "";
+    for (let ingredient of ingredients) {
+        buildString += "<p>" + ingredient + "</p>";
+    }
+    $("#recipe-ingredients").html(buildString);
+}
+
+$("#prev-button").click(function() {
+    if (menuPosition > 0) {
+        --menuPosition;
+        refreshMenuDisplay();
+    }
+});
+
+$("#next-button").click(function() {
+    if (menuPosition < text.menuPages.length - 1) {
+        ++menuPosition;
+        refreshMenuDisplay();
+    }
+});
+
+function getIngredientVal() {
+    var ingredient = $("#new-ingredient").val().trim().toLowerCase();
+    if (ingredient.startsWith("chocolate ")) {
+        return "chocolate";
+    }
+    if (ingredient.endsWith(" cheese")) {
+        return "cheese";
+    }
+    if (ingredient.endsWith(" flour")) {
+        return "flour";
+    }
+    if (ingredient in text.aliases) {
+        return text.aliases[ingredient];
+    }
+    if ((ingredient + "s") in text.examineStrings) {
+        return ingredient + "s";
+    }
+    return ingredient;
+}
+
+function addIngredient() {
+    var ingredient = getIngredientVal();
+    if (ingredient == "") {
+        return;
+    }
+    if (!(ingredient in text.examineStrings)) {
+        $("#recipe-feedback").html("You can&rsquo;t find " + ingredient + " anywhere.");
+        return;
+    }
+    ingredients.push(ingredient);
+    $("#new-ingredient").val("");
+
+    refreshIngredientsDisplay();
+
+    $("#new-ingredient").focus();
+}
+
+$("#ingredient-add").click(addIngredient);
+$("#new-ingredient").keydown(function(event) {
+  if (event.which == 13) {
+      addIngredient();
+  }
+});
+
+function displayIngredientDescription(ingredient) {
+    var ingredientText = "";
+    if (ingredient == "") {
+        return;
+    }
+    if (ingredient in text.examineStrings) {
+        ingredientText = text.examineStrings[ingredient];
+    } else {
+        ingredientText = "You can't find " + ingredient + " anywhere.";
+    }
+    $("#recipe-feedback").html(ingredientText);
+}
+
+$("#ingredient-examine").click(function() {
+    displayIngredientDescription(getIngredientVal());
+});
+
+function identifyRecipe() {
+    for (let recipeName in text.recipes) {
+        var index = 0;
+        var validPrep = true;
+        for (let recipeGroup of text.recipes[recipeName]) {
+            if (index + recipeGroup.length > ingredients.length) {
+                validPrep = false;
+                break;
+            }
+            var sortedRecipeGroup = recipeGroup.slice().sort();
+            var sortedIngredientGroup = ingredients.slice(
+                index, index + recipeGroup.length).sort();
+            if (JSON.stringify(sortedRecipeGroup) != JSON.stringify(sortedIngredientGroup)) {
+                validPrep = false;
+                break;
+            }
+            index += recipeGroup.length;
+        }
+        if (validPrep && index == ingredients.length) {
+            return recipeName;
+        }
+    }
+    return "";
+}
+
+function cook() {
+    if (ingredients.length == 0) {
+        $("#recipe-feedback").html("<p>You can't cook without any ingredients!</p>");
+        return;
+    }
+    var TUTORIAL_RECIPE_STR = "<p><b>Nice job following the tutorial! Now it&rsquo;s time to bake some cookies.</b></p>"
+    var NOT_A_RECIPE_STR = "<p>The waiter looks at your dish and shakes their head, " +
+        "exasperated.</p><p><i>&ldquo;Chom pubba woppzum. Worlik tum.&rdquo;</i></p><p>" +
+        "You throw the dish away and slink back to the kitchen, embarrassed. Looks " +
+        "like you didn&rsquo;t make anything worth serving.</p>";
+    var BASE_RECIPE_STR_PREFIX = "<p>The waiter looks at your dish, confused, and shrugs " +
+        "their shoulders.</p><p><i>&ldquo;Wa pubba brom rolfobben? Ba. Worlik tum.&rdquo;</i></p>" +
+        "<p>You look at your <b>";
+    var BASE_RECIPE_STR_SUFFIX = "</b> and throw it away. You think you were on the right " +
+        "track, but that wasn&rsquo;t a finished dish yet.";
+
+    var SUCCESSFUL_RECIPE_PREFIX = "<p>The waiter looks at your dish animatedly and takes " +
+        "it from you.</p><p><i>&ldquo;Chuunu. Nu pubba.&rdquo;</i></p><p>Nice job! You&rsquo;ve cooked " +
+        "a perfect <b>";
+
+    var COOKIE_PREFIX = "<p>You present your <b>cookies</b> to the waiter. They grunt " +
+      "excitedly, then take them from you and disappear.</p><p>" +
+      "They return a moment later, with a <b>slip of paper</b>. They point to it, indicating " +
+      "that what you seek is written there.</p><p>" +
+      "The paper reads: <b>&ldquo;wepp jarpagom&rdquo;</b>";
+
+    var recipe = identifyRecipe();
+    guessLog.push([ingredients, (recipe == "" ? "nothing good" : recipe)]);
+
+    if (recipe == "crepes") {
+        $("#recipe-feedback").html(TUTORIAL_RECIPE_STR + SUCCESSFUL_RECIPE_PREFIX + recipe + "</b>!</p>");
+    } else if (["pasta", "dough", "sugar dough", "jelly"].includes(recipe)) {
+        $("#recipe-feedback").html(BASE_RECIPE_STR_PREFIX + recipe +
+            BASE_RECIPE_STR_SUFFIX);
+    } else if (recipe == "cookies") {
+        $("#recipe-feedback").html(COOKIE_PREFIX);
+        guessLog.push(["(Received a paper that reads \"wepp jarpagom\".)"]);
+    } else if (recipe != "") {
+        $("#recipe-feedback").html(SUCCESSFUL_RECIPE_PREFIX + recipe + "</b>!</p>");
+    } else {
+        $("#recipe-feedback").html(NOT_A_RECIPE_STR);
+    }
+    ingredients = [];
+    refreshIngredientsDisplay();
+}
+
+function generateLogInfo() {
+  var guessLogString = "<ul>";
+  for (let guess of guessLog) {
+    if (guess.length == 1) {
+      guessLogString += "<li><b>" + guess[0] + "</b></li>";
+    } else {
+      guessLogString += "<li>" + guess[0].join(", ") + " -> " + guess[1] + "</li>";
+    }
+  }
+  guessLogString += "</ul>";
+  $("#modal-guess-log").html(guessLogString);
+}
+$("#recipe-cook").click(cook);
+
+$(".modal-container").hide();
+
+refreshMenuDisplay();
+
+});
